526c114a78cb129cbc9bedf2519d762c640cd76a
Berkan Serbes <berkan_serbes@hotmail.com>
2024-06-20 00:01:53 +0300

    Include middlewares in index.js

--- course-freecodecamp/6-jobs-api/index.js
+++ course-freecodecamp/6-jobs-api/index.js
@@ -5,9 +5,15 @@ const app = express();
 const connectDB = require("./db/connect");
 const authRoute = require("./routers/auth");
 const jobRoute = require("./routers/jobs");
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const authenticateMiddleware = require("./middlewares/authentication");
 
-app.use("/auth", authRoute);
-app.use("/jobs", jobRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/jobs", authenticateMiddleware, jobRoute);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
